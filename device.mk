3421bcec909a856ed11a0b2ba95ee6ba7d729b87
Bruno Martins <bgcngm@gmail.com>
2021-06-19 15:25:31 +0100

    fog: Build opensource soundtrigger HAL
    Change-Id: I956d53a67841a9e1e296de6b73f08ef2efac7470

--- device.mk
+++ device.mk
@@ -49,7 +49,8 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libtinycompress
+    libtinycompress \
+    sound_trigger.primary.bengal
 
 # Audio Daemon
 PRODUCT_PACKAGES += \
