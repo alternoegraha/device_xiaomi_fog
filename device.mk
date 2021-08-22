845f8acddd8093f0950319b5825da217ce360d87
ghostrider-reborn <adithya.r02@outlook.com>
2021-08-22 16:37:18 +0800

    fog: Disable dexpreopt minidebuginfo
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Change-Id: Ief843fc411189906c5d0183a6ff4485f6b3041ee

--- device.mk
+++ device.mk
@@ -413,6 +413,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+WITH_DEXPREOPT_DEBUG_INFO := false
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
