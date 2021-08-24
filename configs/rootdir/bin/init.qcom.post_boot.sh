20f3c66b877cdc8ffbc607e01961682c17c56468
kubersharma001 <kubersharma001@gmail.com>
2021-08-24 13:12:27 +0000

    fog: rootdir: Set swappiness on boot_complete.
    * original commit: https://github.com/ArrowOS-Devices/android_device_xiaomi_vayu/commit/04b08d19aecc3b4840f60d51d116e9714f5c8860
    
    Change-Id: Ie10a18b7d64f10fae954a7de3b2cf34832608fce
    Signed-off-by: boedhack99 <boedhack@gmail.com>

--- configs/rootdir/bin/init.qcom.post_boot.sh
+++ configs/rootdir/bin/init.qcom.post_boot.sh
@@ -1003,7 +1003,6 @@ else
     # Set allocstall_threshold to 0 for all targets.
     # Set swappiness to 100 for all targets
     echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-    echo 100 > /proc/sys/vm/swappiness
 
     # Disable wsf for all targets beacause we are using efk.
     # wsf Range : 1..1000 So set to bare minimum value 1.
