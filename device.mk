df82c751d3cf364fc3e5ebbb6ced29ef067219c5
Mathieu Chartier <mathieuc@google.com>
2021-11-02 21:34:43 +0530

    fog: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- device.mk
+++ device.mk
@@ -410,6 +410,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom-caf/bootctrl \
     hardware/xiaomi
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
