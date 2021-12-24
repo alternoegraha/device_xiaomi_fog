51b7fab624d8878a6749576635a9986ac2e50674
Bowgo Tsai <bowgotsai@google.com>
2021-12-24 11:52:23 +0800

    fog: Remove GSI key includes
    It's no longer required to install the compliance GSI
    public keys into the ramdisk. Those public keys just need
    to be included in the VTS test suite for GSI verification.
    
    Bug: 149806769
    Test: tree hugger
    Those keys will be embedded into VtsSecurityAvb on host side
    instead, to verify the GSI image used on the device.
    
    Bug: 176869478
    Test: TreeHugger
    Change-Id: I3714de75b94be43a1364b89b92d893b336aa014b
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: snnbyyds <snnbyyds@gmail.com>
    Signed-off-by: Tanmsyffa <sltnmsyffaa@gmail.com>
    Signed-off-by: alternoegraha <37102951+alternoegraha@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -7,9 +7,6 @@
 # APEX's
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
-
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
