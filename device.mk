6a3cfe5cd99caac1b18ebaa762a603cd80703932
Bruno Martins <bgcngm@gmail.com>
2022-05-08 01:25:00 +0400

    fog: Migrate to Xiaomi power AIDL HAL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: alternoegraha <37102951+alternoegraha@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -359,8 +359,12 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2.vendor \
-    android.hardware.power-service-qti
+    android.hardware.power@1.3.vendor \
+    android.hardware.power-service.xiaomi-libperfmgr
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # QMI
 PRODUCT_PACKAGES += \
