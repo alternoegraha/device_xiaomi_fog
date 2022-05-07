1853d538c6a0db93a705fbbc41903b27a7c3a48a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-07 15:28:52 +0400

    fog: Stop building vendor.qti.hardware.perf lib
    * We dropped QTI perf stack
    
    Change-Id: Ie71344aa05888adb7ad76ad66693547fc50a0025
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: alternoegraha <37102951+alternoegraha@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -345,14 +345,6 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
-    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
-
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
