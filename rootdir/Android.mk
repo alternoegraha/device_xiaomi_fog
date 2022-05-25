056f9f4325482e629bb669481c2d8523692a57a1
Albert I <kras@raphielgang.org>
2022-05-25 12:57:16 +0400

    fog: Configure zRAM using fstab
    * "32-bit overflow" CAF mentioned in the function wrapper inside post-boot
      script sounds very bs, since if it's 64-bit it won't be affected anyways
    
    * We do the setup using fstab instead to allow setting up more flags such
      as setting up backing device and compression streams; as such kanged from
      Pixels
    
    * zRAM size = 50% physical memory size to satisfy what CAF really wants
    
    * Use LZ4 compression even for regular devices
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Change-Id: Ia6efb3d28e56fb026d9b28fc6633b2eac77dfa47
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -52,6 +52,14 @@ LOCAL_MODULE_PATH  := $(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.zram
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.zram
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
