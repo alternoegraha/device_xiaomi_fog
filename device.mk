a8ef1a3301dd024a6f251419d1c7a26cdc0a662c
boedhack99 <boedhack@gmail.com>
2022-06-05 15:57:06 +0700

    fog: Import NFC init scripts
    Change-Id: Icb054dd5719efc39668f6104b764b8deff516cb1

--- device.mk
+++ device.mk
@@ -44,6 +44,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_c3qn/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_c3qn/com.nxp.mifare.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/init.nfc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.nfc.rc
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
