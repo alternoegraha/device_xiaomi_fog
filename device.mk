8990a85b0d2501de275255927277aa6ab52ccb4a
Albert I <kras@raphielgang.org>
2022-06-19 12:29:52 +0400

    fog:  Inherit several Android Go configurations Beneficial for everyone, even on non-Go devices.
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -236,6 +236,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # IRQ balance
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
