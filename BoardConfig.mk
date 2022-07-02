92a68d7c88c8e2302dd7d0854e48d520cad35452
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-02 11:11:33 +0700

    fog: Add libinit for variant handling
    Change-Id: Iee2f68d40689a9b604a071eff75231603d46b1f7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,10 @@ DEVICE_PATH := device/xiaomi/fog
 # Display
 TARGET_SCREEN_DENSITY := 320
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_fog
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_fog
+
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := fog,rain,wind
 
---
+++ libinit/init_xiaomi_fog.cpp
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_dalvik_heap.h>
+#include <libinit_variant.h>
+
+#include "vendor_init.h"
+
+static const variant_info_t fog_global_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "fog",
+    .marketname = "Redmi 10C",
+    .model = "220333QAG",
+    .build_fingerprint = "Redmi/fog/fog:11/RKQ1.211001.001/V13.0.7.0.RGEMIXM:user/release-keys",
+
+    .nfc = false,
+};
+
+static const variant_info_t fog_global2_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "fog",
+    .marketname = "Redmi 10 Power",
+    .model = "220333QBI",
+    .build_fingerprint = "Redmi/fog_global2/fog:11/RKQ1.211001.001/V13.0.7.0.RGEMIXM:user/release-keys",
+
+    .nfc = false,
+};
+
+static const variant_info_t fog_in_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "fog",
+    .marketname = "Redmi 10",
+    .model = "220333QBI",
+    .build_fingerprint = "Redmi/fog_in/fog:11/RKQ1.211001.001/V13.0.7.0.RGEINXM:user/release-keys",
+
+    .nfc = false,
+};
+
+static const variant_info_t rain_global_info = {
+    .hwc_value = "",
+    .sku_value = "c3qn",
+
+    .brand = "Redmi",
+    .device = "rain",
+    .marketname = "Redmi 10C",
+    .model = "220333QNY",
+    .build_fingerprint = "Redmi/rain_global/rain:11/RKQ1.211001.001/V13.0.7.0.RGEMIXM:user/release-keys",
+
+    .nfc = true,
+};
+
+static const variant_info_t wind_global_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "wind",
+    .marketname = "Redmi 10C",
+    .model = "220333QL",
+    .build_fingerprint = "Redmi/wind_global/wind:11/RKQ1.211001.001/V13.0.7.0.RGEMIXM:user/release-keys",
+
+    .nfc = false,
+};
+
+static const std::vector<variant_info_t> variants = {
+    fog_global_info,
+    fog_global2_info,
+    fog_in_info,
+    rain_global_info,
+    wind_global_info,
+};
+
+void vendor_load_properties() {
+    search_variant(variants);
+    set_dalvik_heap();
+}
