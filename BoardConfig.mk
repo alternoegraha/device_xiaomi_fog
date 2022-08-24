1420c33b3e1f6cf7fa83ad61609f576d1e0af23a
dev-harsh1998 <dev-harsh1998@hotmail.com>
2022-08-24 14:53:54 +0530

    fog: Enable HWUI_COMPILE_FOR_PERF
    * activates few optimizing compiler flags for HWUI
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,9 @@ DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += c3qn
 ODM_MANIFEST_C3QN_FILES := $(DEVICE_PATH)/configs/vintf/manifest_c3qn.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_fog
 TARGET_RECOVERY_DEVICE_MODULES := libinit_fog
