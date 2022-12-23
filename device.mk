621e98d3b9116eec8aa55a06070135cca3a979e8
boedhack99 <boedhack@gmail.com>
2022-12-24 00:11:20 +0700

    fog: Drop non-existent modules in PRODUCT_PACKAGES * Also on sm6225-common * Offending entries: IFAAService android.hardware.light-service.xiaomi android.hardware.media.omx@1.0-impl android.hardware.secure_element@1.2-service audio.primary.bengal hwcomposer.bengal libstagefrighthw_foundation libstagefrighthw_omx q-gsi.avbpubkey r-gsi.avbpubkey s-gsi.avbpubkey vendor.qti.hardware.audiohalext@1.0 vendor.qti.hardware.audiohalext@1.0.vendor vendor.qti.hardware.servicetracker@1.2-service vendor.xiaomi.hardware.mlipay@1.1.vendor vendor.xiaomi.hardware.mtdservice@1.0.vendor
    * Except xiaomi specs hardware modules
    
    Change-Id: Ia80a5d470c84e07cdede77fd774c9c7fad8843b1

--- device.mk
+++ device.mk
@@ -37,7 +37,6 @@ PRODUCT_COPY_FILES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
-    android.hardware.secure_element@1.2-service \
     com.android.nfc_extras \
     libchrome.vendor \
     NfcNci \
