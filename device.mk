598aff1f75b9b369015a09c67f1405a609343f3f
boedhack99 <boedhack@gmail.com>
2023-02-06 22:03:32 +0700

    fog: Switch to source build USB QTI
    Change-Id: I4feeefa90d73ee4d0a86ccdcf234d1e6a1281cf1

--- device.mk
+++ device.mk
@@ -449,8 +449,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.2-service-fog \
-    android.hardware.usb.gadget@1.0-service-fog
+    android.hardware.usb@1.2-service-qti
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
