6f98ce36ad0db6388f54e582d15abad6ed9615e6
boedhack99 <boedhack@gmail.com>
2023-02-21 10:16:18 +0700

    fog: Set compile with MSM kernel
    Change-Id: I48b1179e4d845e2dd9762cbe9e61936f8942c4f9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,7 @@ BOARD_KERNEL_CMDLINE += \
     service_locator.enable=1 \
     swiotlb=2048
 
+TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_KERNEL_CONFIG := vendor/bengal_defconfig
 TARGET_KERNEL_HEADERS := kernel/xiaomi/fog
