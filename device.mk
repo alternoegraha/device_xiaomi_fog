e72119eb5cb151b64bb05dae098dc5a9789bc49c
Ivan Martynov <johnivan19999@gmail.com>
2023-03-03 01:04:34 +0700

    fog: Import and build more QTI Perf system|vendor - This is initial implementation and requires extra rework for stable daily driver - This is needed for WALT kernel usability with proper battery saving - Contains inherit of QTI Frameworks boosts for PE based systems - Added also compilation as vendor.qti.hardware.perf@2.3 , as @2.2 version for non-latest AOSP systems - Added all needed Boot Jars
    Part of changes were taken from changes of following users:
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: xboxfanj <jake@aospa.co>
    Change-Id: I1863d4d7b36e4cf62a407b1bcee11f2a3051051b

--- device.mk
+++ device.mk
@@ -336,14 +336,26 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
 PRODUCT_COPY_FILES += \
+   android.hardware.thermal@2.0 \
+   libpsi.vendor \
+   libtflite \
+   vendor.qti.hardware.perf@2.2.vendor \
+   vendor.qti.hardware.perf@2.3
+
+# Perf Boot Jars
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    QXPerformance \
+    UxPerformance
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
     $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power-service-qti
 
 # Preopted ODEX files (system_other)
 PRODUCT_PACKAGES += \
