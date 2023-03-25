e9bf9570e206955c647ba4b72cd76f305f31dc4e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-03-26 01:49:55 +0700

    fog: props: Enable ADB by default only in non-user builds User builds shouldn't have adb enabled by default.
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: Ia6be823702b47c786f302c67c330c91b973ec68c

--- device.mk
+++ device.mk
@@ -464,6 +464,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.usb.config=mtp,adb
+endif
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
