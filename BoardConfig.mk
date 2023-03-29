b111b426678b27532b4fc963efc9f72e159ce3dc
boedhack99 <boedhack@gmail.com>
2023-03-29 14:23:43 +0700

    fog: Enable NFC with `hwname*` SKUs
    * Only enable for rain users
    
    Change-Id: I44fff5e37360b797cd6f953fc9c35f956c308b1d
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,8 +99,8 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/frame
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
-ODM_MANIFEST_SKUS += nfc
-ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
+ODM_MANIFEST_SKUS += c3qn
+ODM_MANIFEST_C3QN_FILES := $(DEVICE_PATH)/configs/vintf/manifest_c3qn.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_fog
