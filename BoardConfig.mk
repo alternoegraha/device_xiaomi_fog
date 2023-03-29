96a5bdf8e05316629d2703eae0eee262ea4734c1
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-03-29 14:17:02 +0700

    fog: libinit: Update init from AOSPA tree * From https://github.com/CHRISL7/android_device_xiaomi_fog/blob/topaz/init
    Change-Id: I4c6cd85a71d9e1b177885d2336aa9d9da53074ea
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,8 +103,8 @@ ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_fog
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_fog
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_fog
+TARGET_RECOVERY_DEVICE_MODULES := libinit_fog
 
 # Kernel
 BOARD_KERNEL_BASE        := 0x00000000
