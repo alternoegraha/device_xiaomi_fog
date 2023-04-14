eb7e54a7a26717bb45b373c77e6f42b6f87f5759
me-cafebabe <me.cafebabe@gmail.com>
2023-04-14 17:36:25 +0800

    fog: Reduce default DPI size to 280
    * Looks bit better
    
    Change-Id: Id033364df8fa25f74bcd3b22bbea405a0c305732
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -134,7 +134,7 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
 USE_DEX2OAT_DEBUG := false
 
 # Display
-TARGET_SCREEN_DENSITY := 320
+TARGET_SCREEN_DENSITY := 280
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
