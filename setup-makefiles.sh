650c8e9b7ca6eba8f2c4f2d2be1208b022365601
boedhack99 <boedhack@gmail.com>
2023-05-04 01:04:17 +0700

    fixup! setup makefiles tools scripts - I have manually update from lineage scripts - Commit fixup 300ab60
    Change-Id: Ie5be8b117d848c630ba0c327ce2bf8c85ed6a4ba
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -16,7 +16,7 @@ INITIAL_COPYRIGHT_YEAR=2022
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="${MY_DIR}/../.."
+ANDROID_ROOT="${MY_DIR}/../../.."
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
