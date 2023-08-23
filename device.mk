fdd5b5361b6c6bb9606491f867c8a4a37ef66d8b
alternoegraha <37102951+alternoegraha@users.noreply.github.com>
2023-08-23 21:18:18 +0000

    fog: switch again to source built/QTI USB HAL
    Signed-off-by: alternoegraha <37102951+alternoegraha@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -474,7 +474,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.2.vendor:64
+    android.hardware.usb@1.3-service-qti
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
