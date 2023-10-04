0d2c17a8b93f5dbc3cb842831306f4c43c2caf09
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-04 20:36:50 +0530

    fog: Switch to Wifi service AIDL
    Change-Id: I5a2864b4159b7815eff4dd2f0de02a3dabbacfa5
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -507,7 +507,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     android.hardware.wifi.hostapd \
     android.hardware.wifi.supplicant \
     hostapd \
