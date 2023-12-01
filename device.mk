cb601e39dc40507ac545085467bb668bf11eac02
LuK1337 <priv.luk@gmail.com>
2023-12-01 23:24:07 +0100

    fog: Build erofs odm and vendor images
    Making these partitions erofs saves ~550MB, which lets us reserve
    significantly more space on /product.
    
    Change-Id: I88984f6137c0bc9f01da742a7a485445c2162157

--- device.mk
+++ device.mk
@@ -20,7 +20,7 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=ext4 \
+    FILESYSTEM_TYPE_vendor=erofs \
     POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
