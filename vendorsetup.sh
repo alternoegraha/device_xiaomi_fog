d2c327e0fdc1a3600da1525bb1919b4b92a5900e
alternoegraha <noegrahachan@gmail.com>
2023-12-28 04:51:21 +0000

    fog: vendorsetup: update
    Revert commits on kernel side to fix errors during compilation
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,5 +12,9 @@ git clone https://github.com/Asyanx/Sea_Kernel-Fog kernel/xiaomi/fog
 
 # Kernel fixups
 cd kernel/xiaomi/fog
+## Revert "treewide: Fix all coding errors found by GCC 11.2.1"
+git revert --no-commit 91dc89f
+## Revert "kernel: Fix error when compile use GCC"
+git revert --no-commit 7d72ccb
 sed -i 's+R¹.+R0/Upstream+' arch/arm64/configs/vendor/fog-perf_defconfig # rebrand to fix kernel compilation issue during build
 cd ../../../
