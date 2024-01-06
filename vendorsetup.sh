d72d324b6c6a70180dac0174cab49b9c9903f1d9
alternoegraha <noegrahachan@gmail.com>
2024-01-06 12:51:14 +0000

    Revert "fog: vendorsetup: disable kernel commit revert now"
    - Bring it back
    - also revert https://github.com/Asyanx/Sea_Kernel-Fog/commit/ef00d1fcc1edc1427c8224dd144ed5e0a086d024
    
    *additional changes:
    - track fog-r-oss-release branch for kernel
    - rebrand: from R0/Upstream to R0/Amethyst
    
    This reverts commit 814d1f4dbbfe55b1d86229a7f9806895cc92ffda.
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,16 +5,18 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 git clone https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree (Sea kernel by Asyanx)
-git clone https://github.com/Asyanx/Sea_Kernel-Fog kernel/xiaomi/fog
+git clone -b fog-r-oss-release https://github.com/Asyanx/Sea_Kernel-Fog kernel/xiaomi/fog
 
 # Clone my fork of hardware/xiaomi
 # git clone https://github.com/alternoegraha/hardware_xiaomi hardware/xiaomi
 
 # Kernel fixups
 cd kernel/xiaomi/fog
+## Revert Makefile: fix error when compile use clang
+git revert --no-commit ef00d1f
 ## Revert "treewide: Fix all coding errors found by GCC 11.2.1"
-#git revert --no-commit 91dc89f
+git revert --no-commit 91dc89f
 ## Revert "kernel: Fix error when compile use GCC"
-#git revert --no-commit 7d72ccb
-sed -i 's+R¹.+R0/Upstream+' arch/arm64/configs/vendor/fog-perf_defconfig # rebrand to fix kernel compilation issue during build
+git revert --no-commit 7d72ccb
+sed -i 's+R¹.+R0/Amethyst+' arch/arm64/configs/vendor/fog-perf_defconfig # rebrand to fix kernel compilation issue during build
 cd ../../../
