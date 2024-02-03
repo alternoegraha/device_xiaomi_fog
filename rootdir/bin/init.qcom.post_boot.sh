b87da97e8c8bde33974ad2b632a791908af67f87
muralivijay <muralidharan9845@gmail.com>
2024-02-03 19:40:30 +0530

    fog: Increase swappiness upto 200 for 4GB RAM.
    * Increasing more swappiness will allow to use more zram memory. also efficient for 4GB ram users.
    * For 6GB no need increase more swappiness because they can able to do normal mutitask
    * setting 200 swappiness for 4GB RAM will help to do better mutitaksing on android 14.
    
    [alternoegraha]: also apply https://github.com/muralivijay/android_device_xiaomi_spes/commit/79184bcdfa696f4e4829f2d4885eb4e65ca8aa9a
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -50,6 +50,15 @@ function configure_read_ahead_kb_values() {
     fi
 }
 
+# Check for less than 4GB RAM and set 200 swappiness
+    if [ "$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')" -le "4194304" ]; then
+        # Set swappiness to 200
+        echo 200 > /proc/sys/vm/swappiness
+    else
+        # Set swappiness to 100 for more than 4gb RAM
+        echo 100 > /proc/sys/vm/swappiness
+    fi
+
 # Settings for bengal
 echo N > /sys/module/lpm_levels/system/pwr/pwr-l2-gdhs/idle_enabled
 echo N > /sys/module/lpm_levels/system/perf/perf-l2-gdhs/idle_enabled
