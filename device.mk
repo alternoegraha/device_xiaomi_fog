3b34bcb69e0c39f192490688d0e130fc40108c3a
alternoegraha <noegrahachan@gmail.com>
2024-02-12 08:07:58 +0000

    fog: build NoCutoutOverlay
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -342,6 +342,7 @@ PRODUCT_PACKAGES += \
     CarrierConfigFog \
     DialerFog \
     FrameworksFog \
+    NoCutoutOverlay \
     SettingsFog \
     SettingsProviderFog \
     SettingsProviderFogCommon \
