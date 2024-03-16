11ec6b417396d21e9b81fd0ea0e1ab70339f8241
alternoegraha <noegrahachan@gmail.com>
2024-03-16 22:42:51 +0000

    fog: vendorsetup: clone LineageOS' hardware/xiaomi
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,8 +7,8 @@ git clone https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 # Clone kernel tree
 git clone --depth=1 https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
 
-# Clone my fork of hardware/xiaomi
-# git clone https://github.com/alternoegraha/hardware_xiaomi hardware/xiaomi
+# Clone LineageOS' hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 # Kernel fixups
 cd kernel/xiaomi/fog
