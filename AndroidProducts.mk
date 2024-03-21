daf53e113d08cb8c45fff07c5b276cb72d785d6f
alternoegraha <noegrahachan@gmail.com>
2024-03-21 14:00:07 +0700

    fog: update COMMON_LUNCH_CHOICES
    - A14 QPR2 change
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_fog-user \
-    lineage_fog-userdebug \
-    lineage_fog-eng
+    lineage_fog-ap1a-user \
+    lineage_fog-ap1a-userdebug \
+    lineage_fog-ap1a-eng
