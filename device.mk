61a3848242967374a57b64a727ee2f67b011dbb0
alternoegraha <noegrahachan@gmail.com>
2024-03-25 21:26:42 +0000

    fog: Update USB Basic HAL to 1.3
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -479,7 +479,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.basic
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
