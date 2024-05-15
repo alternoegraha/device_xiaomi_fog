5747141cd38c1e036e3fa3cbb1b8fc3df5a7b74b
alternoegraha <noegrahachan@gmail.com>
2024-05-15 22:49:28 +0000

    fog: build android.hardware.media.omx@1.0.vendor
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -253,6 +253,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-service \
+    android.hardware.media.omx@1.0.vendor \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libOmxAacEnc \
