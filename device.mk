7214e526867c50fda0c428a31b30d42a162c3ee8
EmanuelCN <emanuelghub@gmail.com>
2024-06-03 20:19:00 +0000

    fog: Override cgroups.json & task_profiles.json * In order to allow uclamp kernels to place tasks correctly

--- device.mk
+++ device.mk
@@ -115,6 +115,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Configs File System
 PRODUCT_PACKAGES += \
     fs_config_files
