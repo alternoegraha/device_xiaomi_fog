ac4ad1905d9260483df73c77fc6a978ed9db4c99
Vaisakh Murali <mvaisakh@statixos.com>
2024-06-06 20:54:14 +0530

    fog: Move over to AOSP way of shipping kernel
    Change-Id: Id50b6f40e0f6d1e0dc19b21b2aa739363a15c22f
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -241,6 +241,11 @@ PRODUCT_COPY_FILES += \
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
+# Kernel
+LOCAL_KERNEL := device/xiaomi/fog-kernel/Image
+PRODUCT_COPY_FILES += \
+	$(LOCAL_KERNEL):kernel
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
