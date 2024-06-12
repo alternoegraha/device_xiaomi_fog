27f0493b2aedda0e0d69bf3b1d22fe31f96e6945
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    fog: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- device.mk
+++ device.mk
@@ -344,6 +344,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     CarrierConfigFog \
     DialerFog \
+    DeviceAsWebcamFog \
     FrameworksFog \
     FrameworksFogCommon \
     FrameworksFogIndia \
