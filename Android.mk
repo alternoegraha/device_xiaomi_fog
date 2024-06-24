9f2d5576203a39c9f32a8b035dae99258566cc03
Bruno Martins <bgcngm@gmail.com>
2024-06-23 22:13:55 -0300

    fog: Declare EGL libs as symlinks during extraction
    Change-Id: I4b2f3e79b5e518889d9771bf8990e27babca8b61 | AOSP
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- Android.mk
+++ Android.mk
@@ -38,25 +38,6 @@ $(CNE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
 
 # Symlinks
-EGL_LIBRARIES := \
-    libEGL_adreno.so \
-    libGLESv2_adreno.so \
-    libq3dtools_adreno.so
-
-EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(notdir $(EGL_LIBRARIES)))
-$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "EGL 32 lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf egl/$(notdir $@) $@
-
-EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(notdir $(EGL_LIBRARIES)))
-$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "EGL lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf egl/$(notdir $@) $@
-
 ACDBDATA_SYMLINKS := $(TARGET_OUT_ODM)/etc/acdbdata
 $(ACDBDATA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating acdbdata symlinks: $@"
@@ -75,8 +56,6 @@ ALL_DEFAULT_INSTALLED_MODULES += \
     $(BT_FIRMWARE_MOUNT_POINT) \
     $(DSP_MOUNT_POINT) \
     $(CNE_SYMLINKS) \
-    $(EGL_32_SYMLINKS) \
-    $(EGL_64_SYMLINKS) \
     $(ACDBDATA_SYMLINKS) \
     $(WIFI_FIRMWARE_SYMLINKS)
 
