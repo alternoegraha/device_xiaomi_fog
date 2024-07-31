086dfbc1f0b3f35a207696c1dedf0b436646f97e
alternoegraha <noegrahachan@gmail.com>
2024-07-31 09:47:31 +0000

    fog: vendorsetup: update
    - change kernel branch
    - clone forked CertifiedPropsOverlay (by pure-soul-kk)
    - clone PixelOS's hardware/xiaomi
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,10 +5,13 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 git clone https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
-git clone --depth=1 -b reset https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
+git clone --depth=1 -b another-reset https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
 
-# Clone LineageOS' hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+# Clone PixelOS-AOSP hardware/xiaomi
+git clone https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi
+
+# Clone CertifiedPropsOverlay
+git clone https://github.com/pure-soul-kk/vendor_extra vendor/extra
 
 # Kernel fixups
 cd kernel/xiaomi/fog
