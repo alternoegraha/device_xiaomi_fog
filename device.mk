c7aa9166c5e6970aabb000e9ef8e5777840cb4dc
chiteroman <98092901+chiteroman@users.noreply.github.com>
2024-08-01 23:44:04 +0200

    fog: SM8250's TEE does not support device id attestation
    Change-Id: I25dd356311ea17cf741df54f2de0a0152426d847
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -131,10 +131,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
-# Device ID
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
