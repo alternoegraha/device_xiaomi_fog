ca532ffcadf81ade133bd68174923f3c4cb5522b
alternoegraha <noegrahachan@gmail.com>
2024-09-04 10:20:29 +0000

    [WIP] fog: prepare VIC
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_fog-ap2a-user \
-    aosp_fog-ap2a-userdebug \
-    aosp_fog-ap2a-eng
+    aosp_fog-ap3a-user \
+    aosp_fog-ap3a-userdebug \
+    aosp_fog-ap3a-eng
