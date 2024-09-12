38590a05c8fe4da0f25b716802769a127e7bf927
nattolecats <nattolecats@users.noreply.github.com>
2024-09-12 16:38:56 +0530

    fog: Link libwvhidl against libcrypto from vndk 33
    * It depends on symbol CBS_init which is removed from the latest libcrypto.
    
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -175,7 +175,8 @@ PRODUCT_COPY_FILES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libcrypto-v33
 
 # Fastbootd
 PRODUCT_PACKAGES += \
--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,9 @@ function blob_fixup() {
             "${PATCHELF}" --remove-needed "libhidlbase.so" "${2}"
             sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
             ;;
+	    vendor/lib64/libwvhidl.so)
+	        "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
+	        ;;
     esac
 }
 
