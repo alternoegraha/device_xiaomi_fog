acea2d8abc898ff1b9ce6d942fc1c354ecd7af77
alternoegraha <noegrahachan@gmail.com>
2024-09-14 11:36:36 +0000

    fog: move camxoverridesettings to device tree
    - also import camxoverridesettings.txt from MIUI 14.0.3.0.TGEMIXM
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -115,6 +115,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt
+
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
