81bd23a0f575311cfa62c9a13cd0991bae95de93
alternoegraha <noegrahachan@gmail.com>
2024-09-14 11:48:41 +0000

    Revert "fog: vendorsetup: Switch to Luna kernel"
    - my kernel (WWY) has fixed fp gesture bug
    
    This reverts commit 233658e5d2437b4045ee3fa28c4fcedddd474c17.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 git clone https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
-git clone --depth=1 -b luna-staging https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 kernel/xiaomi/fog
+git clone --depth=1 -b another-reset https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
 
 # Clone LineageOS' hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
