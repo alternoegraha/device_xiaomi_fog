fc21b6f1aa55aac0f8c3b4fbba9185dfc8c4bbd0
alternoegraha <noegrahachan@gmail.com>
2024-10-03 22:56:54 +0000

    fog: vendorsetup: update
    - update vendor and kernel branch
    - switch to my own fork of hardware/xiaomi (based on PixelOS' hardware/xiaomi)
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,13 +2,13 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
-git clone -b lineage-21-staging https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
+git clone -b fourteen https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
-git clone --depth=1 -b reset https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
+git clone --depth=1 -b another-reset https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
 
-# Clone LineageOS' hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+# Clone hardware/xiaomi
+git clone https://github.com/alternoegraha/pb_hardware_xiaomi hardware/xiaomi
 
 # Kernel fixups
 cd kernel/xiaomi/fog
