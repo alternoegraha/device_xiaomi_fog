92e54aa0f97b082f02ce3662624d73cf18575960
r0ddty <r0d@ukr.net>
2024-10-28 17:03:11 +0200

    fog: adapt vendorsetup for A15
    Signed-off-by: r0ddty <r0d@ukr.net>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,23 +2,23 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
-git clone https://github.com/r0ddty/vendor_xiaomi_fog vendor/xiaomi/fog
+git clone -b fifteen https://github.com/r0ddty/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
 git clone -b another-main https://github.com/r0ddty/kernel_xiaomi_fog kernel/xiaomi/fog
 
 # Clone LOS hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+git clone -b lineage-22.0 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 # MIUI Camera
 git clone -b leica-5.0 https://gitlab.com/r0ddty/vendor_xiaomi_miuicamera_fog vendor/xiaomi/miuicamera
 
 # OTA
 rm -rf packages/apps/Updater
-git clone https://github.com/r0ddty/android_packages_apps_Updater_fog_unoff packages/apps/Updater
+git clone -b fifteen https://github.com/r0ddty/android_packages_apps_Updater_fog_unoff packages/apps/Updater
 
 rm -rf vendor/risingOTA
-git clone https://github.com/r0ddty/android_vendor_RisingOTA_fog_unoff vendor/risingOTA
+git clone -b fifteen https://github.com/r0ddty/android_vendor_RisingOTA_fog_unoff vendor/risingOTA
 
 # Kernel fixups
 rm kernel/xiaomi/fog/Android.bp
