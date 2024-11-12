2962a47490256baca43f425ca721c3a3d1c60523
r0ddty <r0d@ukr.net>
2024-11-12 20:04:28 +0200

    fog: clone LineageOS sepolicy on vendorsetup
    Signed-off-by: r0ddty <r0d@ukr.net>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,6 +13,9 @@ git clone -b lineage-22.0 https://github.com/LineageOS/android_hardware_xiaomi h
 # MIUI Camera
 git clone -b leica-5.0 https://gitlab.com/r0ddty/vendor_xiaomi_miuicamera_fog vendor/xiaomi/miuicamera
 
+# LineageOS SEpolicy
+git clone -b lineage-22.0 https://github.com/LineageOS/android_device_lineage_sepolicy device/lineage/sepolicy
+
 # Kernel fixups
 rm kernel/xiaomi/fog/Android.bp
 rm kernel/xiaomi/fog/techpack/audio/Android.bp
