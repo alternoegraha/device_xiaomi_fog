ba3b69364b5a5604e53ccbb9287affc28281d528
Iverz <asterixiverz@gmail.com>
2024-11-12 20:36:34 +0200

    fog: change MIUI camera
    Signed-off-by: r0ddty <r0d@ukr.net>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,7 +11,7 @@ git clone -b new https://github.com/r0ddty/kernel_xiaomi_fog kernel/xiaomi/fog
 git clone -b lineage-22.0 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 # MIUI Camera
-git clone -b leica-5.0 https://gitlab.com/r0ddty/vendor_xiaomi_miuicamera_fog vendor/xiaomi/miuicamera
+git clone -b https://gitea.com/asterixiverz/vendor_xiaomi_fog-miuicamera.git vendor/xiaomi/camera
 
 # LineageOS SEpolicy
 git clone -b lineage-22.0 https://github.com/LineageOS/android_device_lineage_sepolicy device/lineage/sepolicy
