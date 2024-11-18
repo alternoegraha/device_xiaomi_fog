dae00f94348e3fadc17536ea0e1035e2cc59715a
npjohnson <johnsonnolen@gmail.com>
2024-11-18 12:54:01 +0545

    fog: Ensure we build the vendor version of libcrypto_shim Fix: F linker  : CANNOT LINK EXECUTABLE /vendor/bin/hw/android.hardware.drm@1.3-service.widevine: library libcrypto_shim.so not found: needed by /vendor/lib64/libwvhidl.so in namespace (default)
    Reference: Ia42de59ff157cddc930a111f8ce5e36e645055ed
    Change-Id: Ie9823e19a86e3066343c010f34f7832b167a2633
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -165,7 +165,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey \
-    libcrypto_shim
+    libcrypto_shim \
+    libcrypto_shim.vendor
 
 # Fastbootd
 PRODUCT_PACKAGES += \
