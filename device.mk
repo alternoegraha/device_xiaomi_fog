94c1a9eecbdd83382abbd2384591bbfaab8af647
Michael Bestas <mkbestas@gmail.com>
2024-11-18 12:52:46 +0545

    fog: Shim widevine with libcrypto_shim The following change google/boringssl@e202e51 removed symbols that our libs require.
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -171,7 +171,8 @@ PRODUCT_COPY_FILES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libcrypto_shim
 
 PRODUCT_PACKAGES += \
     libcrypto_shim \
