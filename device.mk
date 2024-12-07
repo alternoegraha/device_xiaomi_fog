2e059d506429f3ad76eaaa65a9667db7ffab4e42
Nippon <126399437+nnippon99@users.noreply.github.com>
2024-12-07 05:42:54 +0200

    fog: suppress some (more) logcat spam
    Signed-off-by: HeroBuxx <me@herobuxx.me>
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -265,6 +265,9 @@ PRODUCT_PACKAGES += \
 # Logging
 SPAMMY_LOG_TAGS := \
     AnalyticsService \
+    CamX \
+    CHIUSECASE \
+    cnss-daemon \
     KernelCpuUidActiveTimeReader \
     Tracer \
     NearbySharing \
@@ -275,6 +278,7 @@ SPAMMY_LOG_TAGS := \
     b/223498680 \
     TrafficStats \
     ContrastColorUtil \
+    QC-time-services \
     GRALLOC \
     gralloc4 \
     HWUI \
