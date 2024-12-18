7d6f89897f8cd6cd295286782de2989a5e06f3c8
r0ddty <r0d@ukr.net>
2024-12-18 17:40:35 +0200

    fog: use prebuilt Andromeda kernel
    Signed-off-by: r0ddty <r0d@ukr.net>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,10 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 git clone -b fifteen https://github.com/r0ddty/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
-git clone -b new https://github.com/r0ddty/kernel_xiaomi_fog kernel/xiaomi/fog
+git clone -b andromeda https://github.com/r0ddty/kernel_xiaomi_fog kernel/xiaomi/fog
+
+# Clone prebuilt kernel
+git clone -b andromeda https://github.com/r0ddty/device_xiaomi_fog-kernel device/xiaomi/fog-kernel --depth 1
 
 # Clone LOS hardware/xiaomi
 git clone -b lineage-22.0 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
