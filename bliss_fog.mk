c55c6523dbbbfcc74f548fa1fda8fa6d7147e6b1
r0ddty <r0d@ukr.net>
2024-12-18 17:37:56 +0200

    fog: drop Miui camera
    Signed-off-by: r0ddty <r0d@ukr.net>

--- bliss_fog.mk
+++ bliss_fog.mk
@@ -29,9 +29,6 @@ PRODUCT_MODEL := Redmi 10C
 # BlissROMs flags
 TARGET_STOCK_GAPPS := true
 
-# MiuiCamera
-$(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Redmi/fog_global/fog:13/TKQ1.221114.001/V14.0.5.0.TGEMIXM:user/release-keys
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,9 +10,6 @@ git clone -b new https://github.com/r0ddty/kernel_xiaomi_fog kernel/xiaomi/fog
 # Clone LOS hardware/xiaomi
 git clone -b lineage-22.0 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
-# MIUI Camera
-git clone -b leica-5.0 https://gitlab.com/r0ddty/vendor_xiaomi_miuicamera_fog vendor/xiaomi/miuicamera
-
 # LineageOS SEpolicy
 git clone -b lineage-22.0 https://github.com/LineageOS/android_device_lineage_sepolicy device/lineage/sepolicy
 
