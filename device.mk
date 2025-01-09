96e733553851bdec9aa3c67019b36fef84db88df
LuK1337 <priv.luk@gmail.com>
2025-01-09 12:57:53 +0100

    fog: Build gralloc.qcom
    Camera HAL wants it and crashes if it loads default gralloc
    implementation instead.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/8059
    Change-Id: I47965713c822cfdfc369975f8975f21f70133608

--- device.mk
+++ device.mk
@@ -161,6 +161,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
+PRODUCT_PACKAGES += \
+    gralloc.qcom
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
