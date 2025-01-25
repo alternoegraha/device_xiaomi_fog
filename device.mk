625bbbac2a6b3a45db8fdd9eb49785874e9e9388
alternoegraha <noegrahachan@gmail.com>
2025-01-25 09:55:25 +0000

    fog: Rework Bluetooth libraries
    Refrences:
    - https://github.com/LineageOS/android_device_motorola_sm6225-common/blob/17927ae10823f31847c59dd67040ecfe552f3b7c/proprietary-files.txt#L47
    - https://github.com/knyprjkt/android_device_xiaomi_spes/blob/468bc65fe99bf5f56e69d23a8172a0e34488f11f/device.mk#L129
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -69,11 +69,9 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    android.hardware.bluetooth@1.1 \
+    android.hardware.bluetooth@1.1.vendor \
+    liba2dpoffload
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
