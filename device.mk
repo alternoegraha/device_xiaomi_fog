52c9488fc0b39c2124cbb0fdaf3a212f1830e60e
alternoegraha <noegrahachan@gmail.com>
2025-05-15 18:26:42 +0700

    fog: overlay: Drop TetheringOverlay
    - also move networkAttributes and radioAttributes config to framework-res overlay
    - after 3eb57d0f6b0ea9b627fd33e807531c646cb3702f
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -542,7 +542,6 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
-    TetheringOverlayFog \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor \
     wpa_supplicant \
