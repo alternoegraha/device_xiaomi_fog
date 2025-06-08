1702890c10a2035b19e75ad97138bc535062aeb0
Sean Du <do4suki@gmail.com>
2025-06-08 20:59:31 +0800

    fog: Drop neural networks service
    Deprecated in Android 15 [1].
    
    [1] https://developer.android.com/ndk/guides/neuralnetworks/migration-guide
    
    Change-Id: I4e763a544b73a0ac4952a30cb767bb3c85941a52
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -320,10 +320,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
-# Neural Networks
-PRODUCT_PACKAGES += \
-    android.hardware.neuralnetworks@1.3.vendor
-
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
