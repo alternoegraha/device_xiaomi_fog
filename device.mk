f7be0a802ba2e4a3e6a96d2a9d354f7bbbec68b5
Giovanni Ricca <giovanniricca@proton.me>
2025-07-14 10:25:04 +0200

    fog: overlay: Move to OverlayConfig
    Since Android 11, the recommended approach for handling RROs is
    OverlayConfig [1].
    
    Also move the device-specific overlays to the odm partition, because they
    should belong there.
    
    [1]:
    https://source.android.com/docs/core/runtime/rros#configuring-overlays
    
    Change-Id: I4beec15cfa89c88a7ab672af65c9d9f6c358f441
    Co-authored-by: Alexander Winkowski <dereference23@outlook.com>
    [alternoegraha: Adapt to fog]
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -392,6 +392,10 @@ PRODUCT_PACKAGES += \
     NcmTetheringOverlay \
     NoCutoutOverlay
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rro_overlays/config-odm.xml:$(TARGET_COPY_OUT_ODM)/overlay/config/config.xml \
+    $(LOCAL_PATH)/rro_overlays/config-vendor.xml:$(TARGET_COPY_OUT_VENDOR)/overlay/config/config.xml
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
