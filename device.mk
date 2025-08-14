93f287ff1f4edb7cfdd7dbfcc791735bf03fc0ec
fukiame <fukiame@proton.me>
2025-08-15 00:36:04 +0700

    fog: explicitly fry scudo
    from android go config
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Malloc
+PRODUCT_DISABLE_SCUDO := true
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
