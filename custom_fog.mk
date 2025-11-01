9b3b519bffefacd7ee03ab52efcd76bd0bf1b14f
alternoegraha <noegrahachan@gmail.com>
2025-11-01 01:35:05 +0000

    fog: @PixelOS-AOSP time
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- custom_fog.mk
+++ custom_fog.mk
@@ -9,13 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
+TARGET_SUPPORTS_QUICK_TAP := false
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-PRODUCT_NAME := lineage_fog
+PRODUCT_NAME := custom_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
