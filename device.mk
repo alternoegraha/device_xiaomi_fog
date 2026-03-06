39a9db091a02864261cd4de872acb0d7a7599dee
alternoegraha <noegrahachan@gmail.com>
2026-03-06 14:26:23 +0700

    fog: overlay: switch NoCutoutOverlay to device specific ones
    - based on AOSP NoCutoutOverlay, with status_bar_height sizings removed
    - needed to use same status bar height as FrameworksOverlay/SystemUIOverlay instead
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -381,6 +381,7 @@ PRODUCT_PACKAGES += \
     FogFrameworksOverlay \
     FogFrameworksOverlayIN \
     FogFrameworksOverlayIN2 \
+    FogNoCutoutOverlay \
     FogSettingsProviderOverlay \
     FogSettingsProviderOverlayIN \
     FogSettingsProviderOverlayIN2 \
@@ -389,8 +390,7 @@ PRODUCT_PACKAGES += \
     FogWifiOverlayIN2
 
 PRODUCT_PACKAGES += \
-    NcmTetheringOverlay \
-    NoCutoutOverlay
+    NcmTetheringOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rro_overlays/config-odm.xml:$(TARGET_COPY_OUT_ODM)/overlay/config/config.xml \
