3bc94fc271ece1bdff316af7fd57e0618ddf0b7f
alternoegraha <noegrahachan@gmail.com>
2026-05-12 11:32:27 +0700

    fog: use phone-xhdpi-4096 dalvik heap config by default

--- device.mk
+++ device.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
